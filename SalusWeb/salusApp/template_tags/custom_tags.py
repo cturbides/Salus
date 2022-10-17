2949cd850a9250203bc2d88df7e8cc56bcc921c5
cturbides <c.sturbides@gmail.com>
2022-10-17 15:44:37 -0400

    feat: Muscle state logic added

--- SalusWeb/salusApp/template_tags/custom_tags.py
+++ SalusWeb/salusApp/template_tags/custom_tags.py
@@ -8,3 +8,31 @@ def is_here(dictionary: dict, key: str):
 @register.simple_tag
 def get_item(Sensor: Sensors, item: str):
     return getattr(Sensor, item)
+
+@register.simple_tag
+def muscle_state_class(Sensor: Sensors, item: str):
+    muscle_state = int(getattr(Sensor, item))
+    message = "status "
+    
+    if muscle_state < 30:
+        message += "completed"
+    elif muscle_state < 60:
+        message += "process"
+    else:
+        message += "pending"
+    
+    return message
+        
+@register.simple_tag
+def muscle_state_message(Sensor: Sensors, item: str):
+    muscle_state = int(getattr(Sensor, item))
+    message = str()
+    
+    if muscle_state < 30:
+        message = "Relajado"
+    elif muscle_state < 60:
+        message = "Tensos"
+    else:
+        message = "Rígido"
+    
+    return message
